5832ffea29d13b0a5828f1f57d3dc3328f2a9928
sdonich <sdonich@gmail.com>
2019-09-02 22:27:07 +0200

    Add App.test.js

---
+++ src/components/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import {shallow} from 'enzyme';
+import App from './App';
+
+const app = shallow(<App />);
+
+test('renders correctyle', () => {
+  expect(app).toMatchSnapshot();
+});
